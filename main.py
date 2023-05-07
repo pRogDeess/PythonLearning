1c094c5b2e363f84e49661bb069266df9492b41d
pRogDeess <dhillsbest2000@gmail.com>
2023-05-08 03:58:58 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+def example(x):
+    return x
+# printing 10
+print(example(10))
+
+def double(x):
+    print("Hello")
+    return x * 2
+   
+print(double(99))
+
+name = "Vincent"
+print("Hello WOrld, my name is {0}".format(name))
+
+def f(x):
+    return 5 * x - 3
+
+print("I will call function f(x) with 5")
+print(f(5))
+print("Done")
