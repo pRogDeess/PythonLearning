2c2c7f386b46451298655b4302bdfc5afd81b055
pRogDeess <dhillsbest2000@gmail.com>
2023-05-12 03:14:25 +0800

    Create string_1.py

---
+++ string_1.py
@@ -0,0 +1,14 @@
+#String formatting
+print("Hello {0} {1} {2}".format("Vincent", 100, False))
+
+#Followers __ | Following __ | Likes __ 
+follower = 100
+following = 200
+likes = 10
+print("Followers {0} | Following {1} | Likes {2}".format(follower, following, likes))
+follower = 2
+print("Followers {0} | Following {1} | Likes {2}".format(follower, following, likes))
+
+x = "x"
+print(x)
+print(x * 100)
