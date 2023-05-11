35e1415715fd76b4ae90faf9697ae666ac9eab46
pRogDeess <dhillsbest2000@gmail.com>
2023-05-12 03:14:06 +0800

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,14 @@
+
+comment = "Hi love"
+def Ysheen(follower, following, likes):
+    print("Mary Khatynet Ysheen L. Laguna")
+    print("Followers {0} | Following {1} | Likes {2}".format(follower, following, likes))
+
+def CookingRecipe():
+    print("Step 1: Do this")
+    print("Step 2: DO this")
+    print("Step 3: Eat it")
+
+Ysheen(100, 100, 20)
+CookingRecipe()
+print(comment)
