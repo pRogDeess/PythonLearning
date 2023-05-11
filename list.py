041e04a553232a4aca74539e8cea115fb0a586e0
pRogDeess <dhillsbest2000@gmail.com>
2023-05-12 04:26:40 +0800

    Create list.py

---
+++ list.py
@@ -0,0 +1,10 @@
+num = [2, 4, 3, 7, 8, 7, 8, 3, 2]
+print(num)
+print(num[0])
+print((num[4]))
+#0<=index<n - the number of items
+#len shows the n
+print(len(num))
+
+#display the value of the last n in th list
+print(num[len(num) - 1])
