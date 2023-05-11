ea07d5ecebeb0fd87c4cb8a52cf3eff0e1982f96
pRogDeess <dhillsbest2000@gmail.com>
2023-05-12 03:14:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,15 @@ def example(x):
 print(example(10))
 
 def double(x):
-    print("Hello")
+    print("Hello {0}".format(x))
     return x * 2
    
 print(double(99))
 
-name = "Vincent"
-print("Hello WOrld, my name is {0}".format(name))
+def Vincent(name):
+    print("Hello WOrld, my name is {0}".format(name))
+    return name *2
+print(Vincent("Hillst"))
 
 def f(x):
     return 5 * x - 3
